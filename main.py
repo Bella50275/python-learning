ab049a16aea7a2879e9032a8bb20e76d94d09727
Ramirez <bnramirez@cps.k12.il.us>
2024-04-12 12:09:35 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# # #order od excecution 
+# # # in python
+# # print ("hello world")
+# # #strings are surronded by quotes
+# # #single or double quotes " or ""
+# # print ("order of excecution")
+# # print ("in python")
+# # print("*"*20)
+
+# # #variables are used to store data
+# # #variables are created when you assign a value to it
+# # #variables are case sensitive
+# # price=10 #integer variable
+# # name = "John" #stringvariable
+# # rating=  4.9 #float variable
+# # is_published= True #boolean variable
+# # #start all variables with a lower case letter or underscore 
+# # #use underscored to seperate words
+# # # use camel case if you want to start with a capital letter 
+# # #on next word
+# # playerBulls= "michael jordan"
+
+# # #Concatenation to join variables in a sentence
+# # print (name+" is a basketball player")
+# # print (name + " has a rating of" + str(rating))
+# # #use str () function to convert a number to a string
+# # #the plus sign (+) is used to concatenate strings
+# # print(" the price of the book is" + str(price))
+
+# #getting input from the user
+# name=input("What is your name?")
+# age= input("What is your age?")
+# print("Hello"+ name + "you are" + age + "years old")
+#ask two questions from the user
+#person's name and favorite color
+#then print message like "Moses likes blue"
+
+name=input("What is your name?")
+name=input ("What is your favorite color?")
+age=input ("How old are you?")
+age= input ("What is your Birthday?")
+print input(personName + "likes"+ favoriteColor)
+
