4d4c98a47a28eec52a5909f656f24b7fc49536f8
Ramirez <bnramirez@cps.k12.il.us>
2024-04-30 11:57:12 -0500

    first commit

---
+++ day3.py
@@ -0,0 +1,39 @@
+# #booleans
+# print(10 > 9) #is this true or false?
+# #booleans are used to evaluate expressions and conditions in python
+# #they are used to check if an expression is true or false
+# broughtFood = True
+# print(broughtFood)
+
+# is_raining= True
+# if is_raining :
+#     print("Take an umberella !")
+# else:
+#     print("No umbrella.")
+
+# temperature = 15
+# if temperature >30 :
+#     print ("It's warm")
+#     print ("Drink water")
+
+# elif temperature >20:
+#     print("It's nice")
+# else:
+#     print("It's cold")
+#     print("Done")
+
+# age=12
+# message = "Eligiable" if age>= 18 else "Not eligiable"
+# print(message)
+
+# high_income =False
+# good_credit= True
+# student = False
+
+# if(high_income or good_credit) and not student:
+#     print("Eligiable")
+
+# age=22
+# if 18 <= age <65:
+#     print("Eligible")
+
---
+++ day4.py
@@ -0,0 +1,45 @@
+# # # Ask the user to input the current weather. This can be simplified to three categories for ease: "sunny", "rainy", or "cold".
+
+# # weather= input("What is the current weather?(Sunny, rainy, cold):")
+
+
+# # # Implement the decision structure to advise on what to wear:
+
+# # # If the weather is "sunny", recommend "wear sunglasses and a hat".
+# # if weather=="sunny":
+# #     print("wear sunglasses and a hat.")
+
+# # # If the weather is "rainy", recommend "carry an umbrella and wear waterproof boots".
+# # elif weather=="rainy":
+# #     print("Carry an umbrella and wear waterproof boots.")
+# # # If the weather is "cold", recommend "wear a warm coat and gloves".
+# # elif weather=="cold":
+# #     print("Wear a warm coat and gloves.")
+# # # If the input doesn't match any category, inform the user with a message saying the input was not recognized.
+# # else:
+# #     print("Input not recgonzied. Please enter sunny, rainy, or cold.")
+
+# #Next 
+# # Ask the user to input their age and location. Assume location to be either "urban" or "rural".
+# age=int(input("What is your age?"))
+# loaction=input("What is your location? (urban, rural):")
+
+# # Implement the eligibility checks using comparison and logical operators:
+
+
+# # Participants must be at least 18 years old.
+# if age>= 18 and location =="urban":
+#     print("You are eligable to participate.")
+# else:
+#     print("You are not eligiable to particiapte.")
+# # Participants must live in an "urban" area.
+
+# # Display a message indicating whether the user is eligible or not based on these conditions.
+
+# age=int(input("What is your age?"))
+# citizenship=input("What is your citizenship status?(Yes, no):")
+# if age>=18 and citizenship=="Yes":
+#     print("You are eligable to particpate.")
+# else:
+#     print("You are not eligiable to particapate.") 
+
